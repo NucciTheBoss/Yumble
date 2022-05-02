37727880249c737679cf3704fa693a995ee42f0d
Jason Nucciarone <nucci.programming@gmail.com>
2022-05-02 14:59:09 -0400

    Add screen transitions for search for recipe

--- app/src/main/java/com/hcdd340/yumble/activity/SavedRecipesActivity.java
+++ app/src/main/java/com/hcdd340/yumble/activity/SavedRecipesActivity.java
@@ -2,15 +2,23 @@ package com.hcdd340.yumble.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.google.android.material.imageview.ShapeableImageView;
 import com.hcdd340.yumble.R;
+import com.hcdd340.yumble.activity.fragments.SavedRecipesSavedFragment;
+import com.hcdd340.yumble.activity.fragments.SavedRecipesSearchFragment;
+import com.hcdd340.yumble.activity.fragments.SavedRecipesUploadFragment;
 import com.hcdd340.yumble.nav.SwipeListener;
 
-public class SavedRecipesActivity extends AppCompatActivity {
+public class SavedRecipesActivity extends AppCompatActivity implements View.OnClickListener {
     private final static String DEBUG_TAG = "SAVED_RECIPES_ACTIVITY";
 
     ConstraintLayout constraintLayout;
@@ -29,11 +37,46 @@ public class SavedRecipesActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        ShapeableImageView savedButton = findViewById(R.id.recipe_button_saved);
+        ShapeableImageView searchButton = findViewById(R.id.recipe_button_search);
+        ShapeableImageView uploadsButton = findViewById(R.id.recipe_button_uploads);
+        savedButton.setOnClickListener(this);
+        searchButton.setOnClickListener(this);
+        uploadsButton.setOnClickListener(this);
     }
 
     @Override
     public void finish() {
         super.finish();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int resView = view.getId();
+
+        if (resView == R.id.recipe_button_saved) {
+            SavedRecipesSavedFragment savedRecipesSavedFragment = new SavedRecipesSavedFragment();
+            replaceSavedRecipeFragment(savedRecipesSavedFragment);
+
+        } else if (resView == R.id.recipe_button_search) {
+            SavedRecipesSearchFragment savedRecipesSearchFragment = new SavedRecipesSearchFragment();
+            replaceSavedRecipeFragment(savedRecipesSearchFragment);
+
+        } else if (resView == R.id.recipe_button_uploads) {
+            SavedRecipesUploadFragment savedRecipesUploadFragment = new SavedRecipesUploadFragment();
+            replaceSavedRecipeFragment(savedRecipesUploadFragment);
+
+        }
+
+    }
+
+    private void replaceSavedRecipeFragment(Fragment savedRecipeFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out);
+        fragmentTransaction.replace(R.id.saved_recipes_fragment_view, savedRecipeFragment);
+        fragmentTransaction.commit();
     }
 }
